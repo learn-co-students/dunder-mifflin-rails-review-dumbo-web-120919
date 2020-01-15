de317a2dd4ed84c5729dbef56615592b8f7885b3
itslizkim <elizabeth.kim93@gmail.com>
2020-01-15 14:48:59 -0500

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,36 @@
 class DogsController < ApplicationController
+    before_action :find_dog, only: [:show, :edit, :update]
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def new
+        @dog = Dog.new
+    end
+
+    def create 
+        @dog = Dog.create(dog_params)
+        redirect_to dog_path(dog.id)
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+
+    private 
+
+    def dog_params
+        params.require(:dog).permit( :name, :breed, :age)
+    end 
+
+    def find_dog
+        @dog = Dog.find(params[:id])
+    end 
 end
